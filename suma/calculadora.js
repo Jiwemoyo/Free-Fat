68c7e7d9b25c142a70be59a111aef852f289fcbb
Jiwemoyo <97122185+Jiwemoyo@users.noreply.github.com>
2022-03-07 21:07:19 -0500

    suma de calorias

--- suma/calculadora.js
+++ suma/calculadora.js
@@ -4,24 +4,31 @@
 
 function calorias() {
 	var cal = new Number();
-	document.write("Cuantas calorias consumiste",'<BR/>');
-	cal = Number(prompt());
+	let calorias = document.getElementById("cal").value
+	//document.write("Cuantas calorias consumiste",'<BR/>');
+	cal = document.getElementById("cal").value
 	if (cal>=20 && cal<=120) {
-		document.write("Tienes que hacer el ejercicio 1",'<BR/>');
+		document.getElementById("haz").innerHTML="Tienes que hacer el ejercicio 1"
+		//document.write("Tienes que hacer el ejercicio 1",'<BR/>');
 	} else {
 		if (cal>=121 && cal<=312) {
-			document.write("Tienes que hacer el ejercicio 2",'<BR/>');
+			document.getElementById("haz").innerHTML="Tienes que hacer el ejercicio 2"
+			//document.write("Tienes que hacer el ejercicio 2",'<BR/>');
 		} else {
 			if (cal>=313 && cal<=380) {
-				document.write("Tienes que hacer el ejercicio 3",'<BR/>');
+				document.getElementById("haz").innerHTML="Tienes que hacer el ejercicio 3"
+				//document.write("Tienes que hacer el ejercicio 3",'<BR/>');
 			} else {
 				if (cal>=381 && cal<=800) {
-					document.write("Tienes que hacer el ejercicio 4",'<BR/>');
+					document.getElementById("haz").innerHTML="Tienes que hacer el ejercicio 4"
+					//document.write("Tienes que hacer el ejercicio 4",'<BR/>');
 				} else {
 					if (cal>=801) {
-						document.write("Tienes que hacer el ejercicio 5",'<BR/>');
+						document.getElementById("haz").innerHTML="Tienes que hacer el ejercicio 5"
+						//document.write("Tienes que hacer el ejercicio 5",'<BR/>');
 					} else {
-						document.write("No se puede ingresar valores negativos",'<BR/>');
+						document.getElementById("haz").innerHTML="No se aceptan valores negativos"
+						//document.write("No se puede ingresar valores negativos",'<BR/>');
 					}
 				}
 			}
