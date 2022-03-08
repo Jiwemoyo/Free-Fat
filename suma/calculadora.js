b81d3a5ead7cbeddd745c447820e1f49a328222d
Fer :3 <97122618+JimmyLorente@users.noreply.github.com>
2022-03-07 21:07:32 -0500

    jaja

--- suma/calculadora.js
+++ suma/calculadora.js
@@ -1,8 +1,4 @@
-// Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
-// Es posible que el codigo generado no sea completamente correcto. Si encuentra
-// errores por favor reportelos en el foro (http://pseint.sourceforge.net).
 
-function calorias() {
 	var cal = new Number();
 	document.write("Cuantas calorias consumiste",'<BR/>');
 	cal = Number(prompt());
@@ -27,5 +23,4 @@ function calorias() {
 			}
 		}
 	}
-}
 
